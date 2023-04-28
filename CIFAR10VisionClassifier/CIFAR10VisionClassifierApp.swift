05c4e907aadeb62efcbcfd546a1845368ef61a7f
Jose Torronteras <jose.torronteras@bcncgroup.com>
2023-04-28 18:31:01 +0200

    Initial Commit

---
+++ CIFAR10VisionClassifier/CIFAR10VisionClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  CIFAR10VisionClassifierApp.swift
+//  CIFAR10VisionClassifier
+//
+//  Created by Jose Torronteras on 28/4/23.
+//
+
+import SwiftUI
+
+@main
+struct CIFAR10VisionClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CIFAR10VisionClassifier/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CIFAR10VisionClassifier
+//
+//  Created by Jose Torronteras on 28/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
